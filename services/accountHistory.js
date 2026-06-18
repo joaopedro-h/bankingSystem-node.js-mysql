11b5ceb80f40e9e4c9bca9ac09230604ffc4d05d
João Pedro <joao.hermenegildocosta@gmail.com>
2026-06-17 22:14:59 -0300

    refactor: atualiza o histórico para modelo relacional de multiplas tabelas utilizando FOREIGN KEYs

--- services/accountHistory.js
+++ services/accountHistory.js
@@ -6,14 +6,17 @@ async function accountHistory(user,rl,bankingMenu,pause) {
 
         const sqlHistory =  /* Cria a query para realizar a busca do histórico. */
         `SELECT 
-         id_transaction AS "ID da transação",
-         type AS "Tipo",
-         value AS "Valor",
-         user_origin_id AS "ID do usuário",
-         user_destination_id AS "ID do destinatário",
-         date AS "Data"
-         FROM transactions
-        WHERE user_origin_id = ?`;
+            t.type AS "Tipo",
+            t.value AS "Valor",
+            u1.user_name AS "Origem",
+            u2.user_name AS "Destino",
+            date AS "Data"
+        FROM transactions t
+        INNER JOIN users u1
+            ON t.user_origin_id = u1.id
+        INNER JOIN users u2
+            ON t.user_destination_id = u2.id
+        WHERE user_origin_id = ?;`;
 
         const [result] = await connection.execute(sqlHistory,[user.id]);  /* Executa e armazena os rows em result, ignorando os fields retornados pelo MySQL. */
 
